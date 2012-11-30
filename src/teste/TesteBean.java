fa167e0908ea7cada5baea025662b265797a4b89
Tiago Leite <tiagooleite@gmail.com>
2012-11-30 17:26:12 -0300

    Enviando projeto exemplo.

---
+++ src/teste/TesteBean.java
@@ -0,0 +1,24 @@
+package teste;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class TesteBean implements Serializable {
+
+	private static final long serialVersionUID = 5600369132889054255L;
+	
+	private String nome;
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
