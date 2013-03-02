f84cbb07f6d56f85ab7aa88e0e4ea7fc71a1804e
leonardoavelino <leonardoavelino@gmail.com>
2013-03-02 17:01:57 -0300

    make up grafics

--- src/teste/TesteBean.java
+++ src/teste/TesteBean.java
@@ -1,6 +1,9 @@
 package teste;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -12,7 +15,37 @@ public class TesteBean implements Serializable {
 	private static final long serialVersionUID = 5600369132889054255L;
 	
 	private String nome;
-	
+	  
+    private List<String> selectedMovies;  
+  
+    private List<String> selectedOptions;  
+  
+    private Map<String,String> movies;  
+
+    public TesteBean(){
+    	 movies = new HashMap<String, String>();  
+         movies.put("Scarface", "Scarface");  
+         movies.put("Goodfellas", "Goodfellas");  
+         movies.put("Godfather", "Godfather");  
+         movies.put("Carlito's Way", "Carlito's Way");  
+    }
+    public List<String> getSelectedMovies() {  
+        return selectedMovies;  
+    }  
+    public void setSelectedMovies(List<String> selectedMovies) {  
+        this.selectedMovies = selectedMovies;  
+    }  
+  
+    public List<String> getSelectedOptions() {  
+        return selectedOptions;  
+    }  
+    public void setSelectedOptions(List<String> selectedOptions) {  
+        this.selectedOptions = selectedOptions;  
+    }  
+  
+    public Map<String, String> getMovies() {  
+        return movies;  
+    }  
 	public String getNome() {
 		return nome;
 	}
